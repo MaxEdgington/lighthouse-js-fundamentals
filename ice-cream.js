64d9c04251b02fddd68ae734be33c0192e514f18
MaxEdgington <edgington.m.w@gmail.com>
2023-06-08 17:06:59 -0400

    small projects for the LHL js fundamentals, looking at for loops to arrays

---
+++ ice-cream.js
@@ -0,0 +1,8 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+let arrTemp = "root beer"
+let arrNew = [...iceCreamFlavours, arrTemp]
+console.log(arrNew)
+console.log(arrNew[0])
+console.log(arrNew[5])
+console.log(arrNew.length)
